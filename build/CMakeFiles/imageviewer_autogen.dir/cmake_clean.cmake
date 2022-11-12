0a0db24061dcafa14353143f122f742a53f0b9d0
Yue Jiao <cleamoon@hotmail.com>
2022-11-12 18:34:44 +0100

    Remove ignored files

--- build/CMakeFiles/imageviewer_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/imageviewer_autogen"
-  "imageviewer_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/imageviewer_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
